b1ae37dd38363ac54462a952c57ca7b666409b0a
Asma Saleem <asma.saleem@qbatch.com>
2025-11-04 11:06:37 +0500

    Add Celery beat schedule for periodic order summary task

--- fastapi-service/app/celery_app.py
+++ fastapi-service/app/celery_app.py
@@ -14,3 +14,13 @@ celery_app.conf.task_routes = {
     "app.tasks.calculate_order_summary_task": {"queue": "orders"},
     "app.tasks.process_csv_task": {"queue": "uploads"},
 }
+
+celery_app.conf.beat_schedule = {
+    "calculate-orders-every-1-minute": {
+        "task": "app.tasks.calculate_order_summary_task",
+        "schedule": 120.0, 
+        "options": {"queue": "orders"}
+    }
+}
+celery_app.conf.timezone = 'UTC'
+
