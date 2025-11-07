ae4646078f0dda5f9d4ab555c74485da2a47bd90
Asma Saleem <asma.saleem@qbatch.com>
2025-11-07 15:24:17 +0500

    Update the csv logic to load chunk by chunk data in db and memory

--- fastapi-service/app/database.py
+++ fastapi-service/app/database.py
@@ -5,9 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:123@localhost:5432/mydb")
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://d0eeb8c98f78eb415e626b034ae331e92684bbb3c6a9598a44b95b8e0ff69dd4:sk_YLm7IF105XeI_pUoSg5Gp@db.prisma.io:5432/postgres?sslmode=require")
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
